6a6a3f25db8da8f88080a85c07d7f45058ee7baa
gusti3111 <gusti.mispuansyah@gmail.com>
2023-11-24 19:57:30 +0700

    input output stiring correction

--- revConcate.c
+++ revConcate.c
@@ -1,63 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    // inisialisasi variabel
-    char str1[100];
-    char str2[100];
-    char str3[100];
-    char concatenatedStr[300];  // Array untuk menyimpan hasil penggabungan
+int main()
+{
+  // inisialisasi variabel
+  char str1[100];
+  char str2[100];
+  char str3[100];
+  char concatenatedStr[300]; // Array untuk menyimpan hasil penggabungan
 
-    printf("Enter the first string: ");
-    scanf("%s", str1);
+  printf("kalimat 1 = ");
+  scanf("%s", str1);
 
-    printf("Enter the second string: ");
-    scanf("%s", str2);
-
-    printf("Enter the third string: ");
-    scanf("%s", str3);
-
-    // Concatenate Strings
-    strcpy(concatenatedStr, str1);  // Salin str1 ke concatenatedStr
-    strcat(concatenatedStr, " "); // Tambahkan spasi di akhir concatenatedStr
-    strcat(concatenatedStr, str2); // Tambahkan str2 ke akhir concatenatedStr
-
-    strcat(concatenatedStr, " "); // Tambahkan spasi di akhir concatenatedStr
+  printf("kalimat 2 = ");
+  scanf("%s", str2);
   
-    strcat(concatenatedStr, str3);
+  printf("kalimat 3 = ");
+  scanf("%s", str3);
 
-    // Display Concatenated String
-    printf("The concatenated string is: %s\n", concatenatedStr);
+  // Concatenate Strings
+  strcpy(concatenatedStr, str1);
+  strcat(concatenatedStr, " ");
+  strcat(concatenatedStr, str2);
 
-    // Calculate and Display Lengths
-    int length_str1 = strlen(str1);
-    int length_str2 = strlen(str2);
-    int length_str3 = strlen(str3);
-    int length_concatenatedStr = strlen(concatenatedStr);
+  strcat(concatenatedStr, " ");
 
- 
+  strcat(concatenatedStr, str3);
 
-    // Reverse the Concatenated String
-    int length = length_concatenatedStr;
-    int i, j;
-    // Buat array untuk menyimpan karakter-karakter pada concatenatedStr
-    for (i = 0, j = length - 1; i < j; i++, j--) {
-        // Swap characters at positions i and j
-      // 
-        char temp = concatenatedStr[i]; // Simpan karakter pada posisi i pada temp
-      
-        concatenatedStr[i] = concatenatedStr[j]; // Ganti karakter pada posisi i dengan karakter pada posisi j
-        concatenatedStr[j] = temp; // Ganti karakter pada posisi j dengan karakter yang telah disimpan
-      
-    }
+  // Display Concatenated String
+  printf("Gabungan 3 buah inputan kalimat =  %s\n", concatenatedStr);
 
-    // Display Reversed String
-    printf("Reversed string: %s\n", concatenatedStr);
-   printf("Length of str1: %d\n", length_str1);
-    printf("Length of str2: %d\n", length_str2);
-    printf("Length of str3: %d\n", length_str3);
-    printf("Length of concatenatedStr: %d\n", length_concatenatedStr);
+  // Calculate and Display Lengths
+  int length_str1 = strlen(str1);
+  int length_str2 = strlen(str2);
+  int length_str3 = strlen(str3);
+  int length_concatenatedStr = strlen(concatenatedStr);
 
-    return 0;
+  // Reverse the Concatenated String
+  int length = length_concatenatedStr;
+  int i, j;
+  for (i = 0, j = length - 1; i < j; i++, j--)
+  {
+    // Swap characters at positions i and j
+    char temp = concatenatedStr[i];
+    // Ganti karakter pada posisi i dengan karakter pada posisi j
+
+    concatenatedStr[i] = concatenatedStr[j];
+    // Ganti karakter pada posisi j dengan karakter yang telah disimpan
+
+    concatenatedStr[j] = temp;
+  }
+
+  // Display Reversed String
+  printf("Hasil membalik kalimat gabungan = %s\n", concatenatedStr);
+  printf("Jumlah Huruf kalimat pertama = %d\n", length_str1);
+  printf("Jumlah Huruf kalimat kedua = %d\n", length_str2);
+  printf("Jumlah Huruf kalimat ketiga = %d\n", length_str3);
+  printf("Total Jumlah huruf = %d\n", length_concatenatedStr);
+
+  return 0;
 }
-
